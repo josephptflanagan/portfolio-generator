9f0f72c724be41ccb2211c20dd8aea8042b7d3b7
josephptflanagan <josephptflanagan@gmail.com>
2020-07-15 21:12:13 -0500

    using inquire

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
+//const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+//const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const pageHTML = generatePage(name,github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html',generatePage(name,github), err => {
+/*fs.writeFile('index.html', pageHTML, err => {
   if(err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+});*/
+
+const inquirer = require("inquirer");
+
+const promptUser = () => {
+  return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
+  
+promptUser().then(answers => console.log(answers));
